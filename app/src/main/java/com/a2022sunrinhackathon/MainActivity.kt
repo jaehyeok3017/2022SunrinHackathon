d1504c1a250cbe30b71bb1caca4765f1ed9d4189
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-07-15 00:32:18 +0900

    Initial commit

---
+++ app/src/main/java/com/a2022sunrinhackathon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a2022sunrinhackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
