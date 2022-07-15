fc0fc2ee25e77c1662db5a71fe282fba6762a136
nrrr913 <narin2777@naver.com>
2022-07-16 03:08:30 +0900

    Update

---
+++ app/src/main/java/com/a2022sunrinhackathon/Activity/SnsActivity.kt
@@ -0,0 +1,11 @@
+package com.a2022sunrinhackathon.Activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SnsActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
