51419dc61a88d0ab2e864802e49f158d52ec605c
nrrr913 <narin2777@naver.com>
2022-07-15 21:14:24 +0900

    Create : 'AddPhotoActivity'

---
+++ app/src/main/java/com/a2022sunrinhackathon/Activity/AddPhotoActivity.kt
@@ -0,0 +1,64 @@
+package com.a2022sunrinhackathon.Activity
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.a2022sunrinhackathon.R
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage: FirebaseStorage? = null
+    var photoUri: Uri? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        var addPhotoBtnUpload = findViewById<Button>(R.id.add_btn)
+
+        //Initiate storage
+        storage = FirebaseStorage.getInstance()
+
+        //Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        //add image upload event
+        addPhotoBtnUpload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var addPhotoImage = findViewById<ImageView>(R.id.add_image)
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                //path to the selected image
+                photoUri = data?.data
+                addPhotoImage.setImageURI(photoUri)
+            }else{
+                //cancel
+                finish()
+            }
+        }
+    }
+
+
+    fun contentUpload() {
+        //make filename
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+
+
+    }
+
+}
