27bee91f6dac15977c329e999c5873a5b8eb842f
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-13 12:56:34 +0900

    Feat : `SnsActivity.kt`
    - 뒤로가기 버튼을 눌렀을 때, 앱을 종료하도록 변경

--- app/src/main/java/com/a2022sunrinhackathon/activity/SnsActivity.kt
+++ app/src/main/java/com/a2022sunrinhackathon/activity/SnsActivity.kt
@@ -1,7 +1,9 @@
 package com.a2022sunrinhackathon.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.a2022sunrinhackathon.adapter.SnsRecyclerViewAdapter
@@ -14,6 +16,7 @@ class SnsActivity : AppCompatActivity(){
 
     private var firestore : FirebaseFirestore? = null
     private var uid : String? = null
+    private var mBackWait : Long = 0
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,15 @@ class SnsActivity : AppCompatActivity(){
 
         binding.mapBtn.setOnClickListener {
             startActivity(Intent(this, MapActivity::class.java))
+        }
+    }
+
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - mBackWait >=2000 ) {
+            mBackWait = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로가기를 한 번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
+        } else {
+            finishAffinity()
         }
     }
 }
